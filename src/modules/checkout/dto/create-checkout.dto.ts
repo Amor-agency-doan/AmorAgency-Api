6a87b28c031f91cb8688719886e9878190a45896
anhhv1 <vietanh123456123@gmail.com>
2023-08-29 01:08:18 +0700

    prepare dto checkout

--- src/modules/checkout/dto/create-checkout.dto.ts
+++ src/modules/checkout/dto/create-checkout.dto.ts
@@ -1,1 +1,28 @@
-export class CreateCheckoutDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform, Type, plainToClass } from 'class-transformer';
+import { IsMongoId, IsNotEmpty, IsOptional, IsString, ValidateNested } from 'class-validator';
+
+export class UserCheckout {
+  @ApiProperty()
+  @IsNotEmpty({ message: 'ID is required' })
+  fullname: string;
+}
+
+export class ProductCheckoit {
+  @IsMongoId({ message: 'ID is not match' })
+  @IsNotEmpty({ message: 'ID is required' })
+  @IsOptional()
+  id: string;
+
+  @ApiProperty()
+  @IsNotEmpty({ message: 'Quantity is required' })
+  quantity: number;
+}
+export class CreateCheckoutDto {
+  @ApiProperty({ type: UserCheckout })
+  user: UserCheckout;
+
+  @ApiProperty({ type: ProductCheckoit, isArray: true })
+  @IsOptional()
+  products: string;
+}
